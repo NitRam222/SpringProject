88de6f1f26f63de9566857f39da7dbaab59acbaf
Martin <158082290+NitRam222@users.noreply.github.com>
2024-11-18 08:10:41 +0100

    Add files via upload

---
+++ SpringController/java-spring/src/main/java/com/example/Car1.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Car1 {
+	private final String model;
+	private double price;
+}
